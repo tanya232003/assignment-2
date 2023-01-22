22c5c38596f64cf21d198a1657a3febd2a28f889
Tanya <121035554+tanya232003@users.noreply.github.com>
2023-01-22 22:37:21 +0530

    Update ques_17.py

--- ques_17.py
+++ ques_17.py
@@ -2,10 +2,11 @@ x = 5
 
 for i in range(x):
     for j in range(x):
-        if ((i==x//2) or (j==x//2)):
+        if j>=x//2 and (j<=x//2 +i) and (i+j<=x+x//2 -1) :
             print("*   ",end='')
-        elif((j-i)%2==0 and j>x//2 and j<x-1):
+        elif i==x//2:
             print("*   ",end='')
         else:
-            print("    ",end='')
+            print("    ",end='') 
+            
     print()
