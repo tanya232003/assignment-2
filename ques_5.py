b2b5bb9d1e1570f257f86dd7d2e84084872f375f
Tanya <121035554+tanya232003@users.noreply.github.com>
2023-01-22 22:10:32 +0530

    Update ques_5.py

--- ques_5.py
+++ ques_5.py
@@ -1,18 +1,8 @@
-x = 5 
-
+x=5
 for i in range(x):
-    if i<x//2+1:
-        for j in range(x):
-            if j>=x-3-i and j<=x-3+i :
-                print("*   ",end='')
-            else:
-                print("    ",end='')
-        print()
-        
-    if i >= x//2+1 and i < x:
-        for j in range(x):
-            if j>=i-x+3 and j<=x+1-i :
-                print("*   ",end='')
-            else:
-                print("    ",end='')
-        print()
+    for j in range(x):
+        if (j<=x//2 +i) and (i<=x//2 +j) and (i+j>=x-x//2 -1) and (i+j<=x+x//2 -1) :
+            print("*",end='')
+        else:
+            print(" ",end='')
+    print()
