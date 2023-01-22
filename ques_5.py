32827abd7c9f383f186a7d9388c0f667557ceded
Tanya <121035554+tanya232003@users.noreply.github.com>
2023-01-22 22:23:00 +0530

    Update ques_5.py

--- ques_5.py
+++ ques_5.py
@@ -2,7 +2,7 @@ x=5
 for i in range(x):
     for j in range(x):
         if (j<=x//2 +i) and (i<=x//2 +j) and (i+j>=x-x//2 -1) and (i+j<=x+x//2 -1) :
-            print("*",end='')
+            print("*   ",end='')
         else:
-            print(" ",end='')
+            print("    ",end='')
     print()
